acfc5598c0650d4dc7d47890f962890824cfbebb
wantg <wantg78@gmail.com>
2024-10-09 01:17:52 +0800

    0207 Enhanced Input

---
+++ Source/Aura/Player/AuraPlayerController.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AuraPlayerController.generated.h"
+
+UCLASS()
+class AURA_API AAuraPlayerController : public APlayerController {
+    GENERATED_BODY()
+};
