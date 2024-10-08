c5842d99299b5a5076456428c39a3d409d803709
wantg <wantg78@gmail.com>
2024-10-09 00:30:55 +0800

    0204 Player and Enemy Characters

---
+++ Source/Aura/Character/AuraCharacterBase.cpp
@@ -0,0 +1,9 @@
+#include "Character/AuraCharacterBase.h"
+
+AAuraCharacterBase::AAuraCharacterBase() {
+    PrimaryActorTick.bCanEverTick = false;
+}
+
+void AAuraCharacterBase::BeginPlay() {
+    Super::BeginPlay();
+}
