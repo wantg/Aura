3c8ac2379bff8ab2ce8696e284998910f4166e5e
wantg <wantg78@gmail.com>
2024-10-09 02:28:24 +0800

    0304 Ability System Component and Attribute Set

---
+++ Source/Aura/AbilitySystem/AuraAbilitySystemComponent.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
+#include "AuraAbilitySystemComponent.generated.h"
+
+UCLASS()
+class AURA_API UAuraAbilitySystemComponent : public UAbilitySystemComponent {
+    GENERATED_BODY()
+};
