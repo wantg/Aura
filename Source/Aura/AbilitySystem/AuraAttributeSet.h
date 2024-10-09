e907f78de0845def3469563918c5de65699248ba
wantg <wantg78@gmail.com>
2024-10-09 12:58:12 +0800

    0610 PreAttributeChange

--- Source/Aura/AbilitySystem/AuraAttributeSet.h
+++ Source/Aura/AbilitySystem/AuraAttributeSet.h
@@ -19,6 +19,8 @@ class AURA_API UAuraAttributeSet : public UAttributeSet {
     UAuraAttributeSet();
     virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 
+    virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
+
     UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Health, Category = "Vital Attributes")
     FGameplayAttributeData Health;
     ATTRIBUTE_ACCESSORS(UAuraAttributeSet, Health);
