9b0ba022344c595241bfc4c206a52397752b0fd7
wantg <wantg78@gmail.com>
2024-10-09 12:40:37 +0800

    0608 Instant and Duration Application Policy

--- Source/Aura/Actor/AuraEffectActor.cpp
+++ Source/Aura/Actor/AuraEffectActor.cpp
@@ -24,6 +24,20 @@ void AAuraEffectActor::ApplyEffectToTarget(AActor* TargetActor, TSubclassOf<UGam
     TargetASC->ApplyGameplayEffectSpecToSelf(*EffectSpecHandle.Data.Get());
 }
 
-void AAuraEffectActor::OnOverlap(AActor* TargetActor) {}
+void AAuraEffectActor::OnOverlap(AActor* TargetActor) {
+    if (InstantEffectApplicationPolicy == EEffectApplicationPolicy::ApplyOnOverlap) {
+        ApplyEffectToTarget(TargetActor, InstantGameplayEffectClass);
+    }
+    if (DurationEffectApplicationPolicy == EEffectApplicationPolicy::ApplyOnOverlap) {
+        ApplyEffectToTarget(TargetActor, DurationGameplayEffectClass);
+    }
+}
 
-void AAuraEffectActor::OnEndOverlap(AActor* TargetActor) {}
+void AAuraEffectActor::OnEndOverlap(AActor* TargetActor) {
+    if (InstantEffectApplicationPolicy == EEffectApplicationPolicy::ApplyOnEndOverlap) {
+        ApplyEffectToTarget(TargetActor, InstantGameplayEffectClass);
+    }
+    if (DurationEffectApplicationPolicy == EEffectApplicationPolicy::ApplyOnEndOverlap) {
+        ApplyEffectToTarget(TargetActor, DurationGameplayEffectClass);
+    }
+}
