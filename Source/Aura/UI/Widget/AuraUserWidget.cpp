20433d2cd8b2ad429315c76e90b50b6071352952
wantg <wantg78@gmail.com>
2024-10-09 09:10:06 +0800

    0502 Aura User Widget and Widget Controller

---
+++ Source/Aura/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,6 @@
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController) {
+    WidgetController = InWidgetController;
+    WidgetControllerSet();
+}
