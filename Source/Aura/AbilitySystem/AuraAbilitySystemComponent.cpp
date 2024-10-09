88d3b0b43839b5234bab6fd0048241a8e12405af
wantg <wantg78@gmail.com>
2024-10-09 19:30:45 +0800

    0708 Broadcasting Effect Asset Tags

--- Source/Aura/AbilitySystem/AuraAbilitySystemComponent.cpp
+++ Source/Aura/AbilitySystem/AuraAbilitySystemComponent.cpp
@@ -8,9 +8,6 @@ void UAuraAbilitySystemComponent::EffectApplied(UAbilitySystemComponent* Ability
     const FGameplayEffectSpec& EffectSpec, FActiveGameplayEffectHandle ActiveEffectHandle) {
     FGameplayTagContainer TagContainer;
     EffectSpec.GetAllAssetTags(TagContainer);
-    for (const FGameplayTag& Tag : TagContainer) {
-        // TODO: Broadcast the tag to the Widget Controller
-        const FString Msg = FString::Printf(TEXT("GE Tag: %s"), *Tag.ToString());
-        GEngine->AddOnScreenDebugMessage(-1, 8.f, FColor::Blue, Msg);
-    }
+
+    EffectAssetTags.Broadcast(TagContainer);
 }
--- Source/Aura/UI/WidgetController/OverlayWidgetController.cpp
+++ Source/Aura/UI/WidgetController/OverlayWidgetController.cpp
@@ -1,5 +1,6 @@
 #include "UI/WidgetController/OverlayWidgetController.h"
 
+#include "AbilitySystem/AuraAbilitySystemComponent.h"
 #include "AbilitySystem/AuraAttributeSet.h"
 
 void UOverlayWidgetController::BroadcastInitialValues() {
@@ -29,6 +30,13 @@ void UOverlayWidgetController::BindCallbacksToDependencies() {
     AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(
                               AuraAttributeSet->GetMaxManaAttribute())
         .AddUObject(this, &UOverlayWidgetController::MaxManaChanged);
+
+    Cast<UAuraAbilitySystemComponent>(AbilitySystemComponent)->EffectAssetTags.AddLambda([](const FGameplayTagContainer& AssetTags) {
+        for (const FGameplayTag& Tag : AssetTags) {
+            const FString Msg = FString::Printf(TEXT("GE Tag: %s"), *Tag.ToString());
+            GEngine->AddOnScreenDebugMessage(-1, 8.f, FColor::Blue, Msg);
+        }
+    });
 }
 
 void UOverlayWidgetController::HealthChanged(const FOnAttributeChangeData& Data) const {
