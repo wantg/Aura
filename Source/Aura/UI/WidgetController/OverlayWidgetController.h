4d98f25d01117fd181c88482c88789b4b3389519
wantg <wantg78@gmail.com>
2024-10-09 19:54:52 +0800

    0710 Retrieving Rows from Data Tables

--- Source/Aura/UI/WidgetController/OverlayWidgetController.h
+++ Source/Aura/UI/WidgetController/OverlayWidgetController.h
@@ -56,4 +56,12 @@ class AURA_API UOverlayWidgetController : public UAuraWidgetController {
     void MaxHealthChanged(const FOnAttributeChangeData& Data) const;
     void ManaChanged(const FOnAttributeChangeData& Data) const;
     void MaxManaChanged(const FOnAttributeChangeData& Data) const;
+
+    template <typename T>
+    T* GetDataTableRowByTag(UDataTable* DataTable, const FGameplayTag& Tag);
 };
+
+template <typename T>
+T* UOverlayWidgetController::GetDataTableRowByTag(UDataTable* DataTable, const FGameplayTag& Tag) {
+    return DataTable->FindRow<T>(Tag.GetTagName(), TEXT(""));
+}
