efe3f1195b88d9634db6a9e4e0c39afe6adb6ae6
wantg <wantg78@gmail.com>
2024-10-09 20:02:27 +0800

    0711 Broadcasting Data Table Rows

--- Source/Aura/UI/WidgetController/OverlayWidgetController.h
+++ Source/Aura/UI/WidgetController/OverlayWidgetController.h
@@ -4,13 +4,6 @@
 #include "UI/WidgetController/AuraWidgetController.h"
 #include "OverlayWidgetController.generated.h"
 
-class UAuraUserWidget;
-
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnHealtChangedSignature, float, NewHealth);
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMaxHealtChangedSignature, float, NewMaxHealth);
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnManaChangedSignature, float, NewMana);
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMaxManaChangedSignature, float, NewMaxMana);
-
 USTRUCT(BlueprintType)
 struct FUIWidgetRow : public FTableRowBase {
     GENERATED_BODY()
@@ -22,11 +15,20 @@ struct FUIWidgetRow : public FTableRowBase {
     FText Message = FText();
 
     UPROPERTY(EditAnywhere, BlueprintReadOnly)
-    TSubclassOf<UAuraUserWidget> MessageWidget;
+    TSubclassOf<class UAuraUserWidget> MessageWidget;
 
     UPROPERTY(EditAnywhere, BlueprintReadOnly)
     UTexture2D* Image = nullptr;
 };
+
+class UAuraUserWidget;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnHealtChangedSignature, float, NewHealth);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMaxHealtChangedSignature, float, NewMaxHealth);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnManaChangedSignature, float, NewMana);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMaxManaChangedSignature, float, NewMaxMana);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FMessageWidgetRowSignature, FUIWidgetRow, Row);
 
 UCLASS(BlueprintType, Blueprintable)
 class AURA_API UOverlayWidgetController : public UAuraWidgetController {
@@ -47,6 +49,9 @@ class AURA_API UOverlayWidgetController : public UAuraWidgetController {
 
     UPROPERTY(BlueprintAssignable, Category = "GAS|Attributes")
     FOnMaxManaChangedSignature OnMaxManaChanged;
+
+    UPROPERTY(BlueprintAssignable, Category = "GAS|Messages")
+    FMessageWidgetRowSignature MessageWidgetRowDelegate;
 
    protected:
     UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Widget Data")
