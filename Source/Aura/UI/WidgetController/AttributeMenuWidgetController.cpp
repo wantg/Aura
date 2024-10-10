35a1a80c82af21f5cac5de8b5d25ba0d1893bfac
wantg <wantg78@gmail.com>
2024-10-11 00:47:07 +0800

    0915 Attribute Menu Widget Controller

---
+++ Source/Aura/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,5 @@
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies() {}
+
+void UAttributeMenuWidgetController::BroadcastInitialValues() {}
