c4cfbc73d0aeead6255cf52bc8a205eabcf16243
wantg <wantg78@gmail.com>
2024-10-11 00:13:15 +0800

    0912 Aura Asset Manager

--- Source/Aura/AuraGameplayTags.h
+++ Source/Aura/AuraGameplayTags.h
@@ -14,6 +14,8 @@ struct FAuraGameplayTags {
     static const FAuraGameplayTags& Get() { return GameplayTags; }
     static void InitializeNativeGameplayTags();
 
+    FGameplayTag Attributes_Secondary_Armor;
+
    protected:
    private:
     static FAuraGameplayTags GameplayTags;
