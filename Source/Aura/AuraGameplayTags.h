b3979b83382baef8eca2a95dfbc1f61f1b8b14d9
wantg <wantg78@gmail.com>
2024-10-15 01:36:14 +0800

    1004 Input Config Data Asset

--- Source/Aura/AuraGameplayTags.h
+++ Source/Aura/AuraGameplayTags.h
@@ -30,7 +30,13 @@ struct FAuraGameplayTags {
     FGameplayTag Attributes_Secondary_MaxHealth;
     FGameplayTag Attributes_Secondary_MaxMana;
 
-   protected:
+    FGameplayTag InputTag_LMB;
+    FGameplayTag InputTag_RMB;
+    FGameplayTag InputTag_1;
+    FGameplayTag InputTag_2;
+    FGameplayTag InputTag_3;
+    FGameplayTag InputTag_4;
+
    private:
     static FAuraGameplayTags GameplayTags;
 };
