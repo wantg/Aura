f05442f52153d0d09066675198005751b328f223
wantg <wantg78@gmail.com>
2024-10-15 02:30:41 +0800

    1014 Aura Projectile Spell

---
+++ Source/Aura/AbilitySystem/Abilities/AuraProjectileSpell.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/AuraGameplayAbility.h"
+#include "AuraProjectileSpell.generated.h"
+
+UCLASS()
+class AURA_API UAuraProjectileSpell : public UAuraGameplayAbility {
+    GENERATED_BODY()
+
+   protected:
+    virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+};
